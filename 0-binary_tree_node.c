5f1f67a1c3d982a5d3a8b2677b622e8c52a19353
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-02-27 21:16:08 +0300

    mm

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h";
+/**
+ * binary_tree_node: ...
+ * @parent: ...
+ * @value: ...
+ * Return: ...
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
+    if (newnode == NULL)
+    {
+        return NULL;
+    }
+    newnode->parent = value;
+    newnode->left = NULL;
+    newnode->right = NULL;
+    return newnode;
+};
