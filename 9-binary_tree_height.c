be8e5660d339da2bf96369b54ed915d4e2ff53b2
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-02-28 02:21:53 +0300

    mm

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - ...
+ * @tree: ...
+ * Return: ...
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t lefth, righth;
+    if (tree == NULL)
+        return (0);
+    else{
+        lefth = binary_tree_height(tree->left);
+        righth = binary_tree_height(tree->right);
+    }
+    if(lefth >= righth)
+        return (lefth + 1);
+    else if (righth >= lefth)
+        return (righth + 1);
+    else
+        return (0);
+}
