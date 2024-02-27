40a835709816968df385d7a6b79eceb9512479bf
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-02-28 00:33:59 +0300

    mm

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -4,13 +4,15 @@
  * binary_tree_delete - ...
  * @tree: ...
  * Return: ...
-*/
+ */
 void binary_tree_delete(binary_tree_t *tree)
 {
     if (tree == NULL)
         return;
     else
+    {
         binary_tree_delete(tree->left);
         binary_tree_delete(tree->right);
         free(tree);
+    }
 }
