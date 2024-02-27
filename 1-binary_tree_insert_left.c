9993153eca384d10f75ef363edbbaa9fd06b9ecf
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-02-27 22:18:12 +0300

    mm

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,11 +8,11 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+    binary_tree_t *newnode = NULL;
     if (parent == NULL)
     {
         return (NULL);
     }
-    binary_tree_t *newnode = NULL;
     newnode = malloc(sizeof(binary_tree_t));
     if (newnode == NULL)
     {
