b3a8454fb668fabbf502c56d8d7f74683ac0443f
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-02-28 22:33:03 +0300

    mm

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,6 +8,6 @@
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
     if (tree == NULL) return (0);
-    if (tree->left == NULL && tree->right) return (0);
+    if (tree->left == NULL && tree->right == NULL) return (0);
     else return binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1;
 }
