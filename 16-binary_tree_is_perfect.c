0b16774db5dbb549080f0faf5062dcfbb220445d
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-02-28 23:51:47 +0300

    mm

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -29,7 +29,21 @@ size_t binary_tree_height(const binary_tree_t *tree)
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+    size_t height = 0;
     if (tree == NULL) return (0);
-    else if (binary_tree_height(tree->left) == binary_tree_height(tree->right)) return (1);
+    height = binary_tree_height(tree);
+    if (binary_tree_size == ((1 << (height + 1)) - 1)) return (1);
     else return (0);
+}
+
+/**
+ * binary_tree_size - ...
+ * @tree: ...
+ * Return: ...
+*/
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+    if (tree == NULL) return (0);
+    else
+        return (binary_tree_size(tree->right) + binary_tree_size(tree->left) + 1);
 }
