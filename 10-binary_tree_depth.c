eeae48e310ba96c01678b1513f0e334318fcee54
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-02-28 22:04:19 +0300

    mm

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -4,16 +4,19 @@
  * binary_tree_depth - ...
  * @tree: ...
  * Return: ...
-*/
+ */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
     int depth = 0;
     if (tree == NULL)
         return (0);
     else
-        while(tree->parent){
+    {
+        while (tree->parent)
+        {
             tree = tree->parent;
             depth++;
         }
         return (depth);
+    }
 }
