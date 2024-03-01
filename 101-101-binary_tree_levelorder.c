e991c4c53b5d2f1df0c9fa94ac0e3f6b6981bffd
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-01 22:04:15 +0300

    mm

---
+++ 101-101-binary_tree_levelorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_levelorder - ...
+ * @tree: ...
+ * @func: ...
+ * Return: ...
+*/
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (tree != NULL && func != NULL)
+	{
+        func(tree->n);
+		binary_tree_levelorder(tree->left, func);
+		binary_tree_levelorder(tree->right, func);
+	}
+    else
+        return;
+}
