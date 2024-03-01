b518796d61bb50da6bcb2ea1127d4d767fb8efb4
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-01 21:43:40 +0300

    mm

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,20 @@
+#include"binary_trees.h"
+
+/**
+ * binary_trees_ancestor - ...
+ * @first: ...
+ * @second: ...
+ * Return: ...
+*/
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+    const binary_tree_t* af, *as;
+    if (first == NULL || second == NULL) return (NULL);
+    while (first != NULL){
+        while(second != NULL){
+            if (af == as) return (as);
+            as = second->parent;
+        }
+        af = first->parent;
+    }
+}
